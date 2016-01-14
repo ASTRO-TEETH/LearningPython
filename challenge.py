432ee8a169270bcbe78a5b8d8bbc455e5d2320e3
Dhiresh Nathwai <dhireshnathwani@gmail.com>
2016-01-14 17:19:28 +0000

    All python programmes

---
+++ challenge.py
@@ -0,0 +1,18 @@
+#*python challenge task 1*
+
+print ("Twinkle Twinkle Little Star,\n \t How i wonder what you are, \n \t \t Up above the world so high! \n \t \t Like the diamond in the sky! \n Twinkle Twinkle little star \n \t How i wonder what you are ")
+
+#*python challenge task 2*
+
+print ("What is your first name?")
+firstname = input()
+print ("What is your surname?")
+surname = input()
+
+fullname = surname +" "+ firstname
+print ("your name is" ,fullname)
+
+#*python challenge task 3*
+
+colours_list = ["red" , "yellow" , "orange" , "black" , "purple"]
+print (" [0] [4],", colours_list)
